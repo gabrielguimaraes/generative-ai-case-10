5f1806c3b357764f1f602ed0431c68783660a81b
Gabriel Guimaraes <gabguim@gmail.com>
2023-09-20 21:03:37 +0200

    UC#10 Infrastructure setup

---
+++ src/main/java/com/gabrielguimaraes/usecase10/App.java
@@ -0,0 +1,13 @@
+package com.gabrielguimaraes.usecase10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/gabrielguimaraes/usecase10/AppTest.java
@@ -0,0 +1,12 @@
+package com.gabrielguimaraes.usecase10;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void simpleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
